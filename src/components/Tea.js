270e232121af021ed618c650c793a3546227fdc9
Tyler Emmerson <tyleremmerson0216@gmail.com>
2022-09-21 21:03:43 -0700

    add shadowing and further css to cards.

--- src/components/Tea.js
+++ src/components/Tea.js
@@ -1,14 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const cardStyles = {
+  container: {
+    display: "flex",
+    height: 130,
+    width: 130,
+    boxShadow: "10px 0 30px 0 #000000",
+    color: "Red",
+    alignItems: "center",
+    padding: 20,
+    borderRadius: 200,
+    marginLeft: 30,
+    marginBottom: 30,
+  },
+  profilePicture: {
+    // display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "Green",
+    color: "Black",
+    height: 100,
+    width: 100,
+    borderRadius: "50%",
+    padding: 35,
+    fontWeight: "bold",
+    
+  },
+  bio: {
+    marginLeft: 10,
+  },
+  userName: {
+    fontWeight: "bold",
+  },
+};
 function Tea(props){
 
   return (
     <React.Fragment>
-      <div onClick = {() => props.whenTeaClicked(props.id)}>
-        <h3 className="listName">{props.name} Tea - From {props.origin}</h3>
-        <h3>{props.price}</h3>
-        <h5> {props.inventory} servings left</h5>
+      <div style={cardStyles.container}>
+        <div onClick = {() => props.whenTeaClicked(props.id)}>
+          <div style={cardStyles.profilePicture}>
+            <span style={cardStyles.userName} className="listName">{props.name} Tea - From {props.origin}</span>
+            <h3>{props.price}</h3>
+            <h5> {props.inventory} servings left</h5>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
